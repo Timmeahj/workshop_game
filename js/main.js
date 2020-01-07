dbf15a8bbda8d4f466aeaddf58d0c6c1e89db69f
Timmeahj <broostim@hotmail.be>
2020-01-07 15:59:34 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,221 +1,297 @@
-const barHeight = getHeight(document.getElementById('bar'));
-let width = getWidth(window);
-let height = getHeight(window) - barHeight;
-
 let score = 0;
 let lives = 3;
-let damaged = false;
-let collision = setInterval(findCollision, 50);
+let target = document.getElementById('target');
+let immune = false;
+let gotPoint = false;
 
-setScore();
-setLives();
+let mouseX = 0;
+let mouseY = 0;
 
-const target = document.getElementById('target');
-const dead = document.getElementById('dead');
-let enemies = document.getElementsByClassName('enemy');
+let spawnEnemyInterval = 10;
+let spawnLifeInterval = 25;
+let spawnSnitchInterval = 100;
+let spawnShieldInterval = 11;
 
-let winScore = 350;
-let targetSpeed = 0.3;
-let enemySpeed = 1.2;
-let deadSpeed = 1.5;
-let interval = 900;
-let spawnDeadWhen = 75;
-let spawnInterval = 30;
+let spawnBoss = 50;
 
-target.style.transition = targetSpeed+"s";
+let shieldTime = 5000;
 
-dead.style.transition = deadSpeed+"s";
-let death = setInterval(move, interval, dead);
+initialise();
 
-let mouseX = 1000;
-let mouseY = 1000;
-
-document.onmousemove = findMouse;
-
-function findMouse(e){
-  mouseX = e.pageX+15;
-  mouseY = e.pageY;
-  document.getElementById('cursor').style.marginTop = mouseY-60+"px";
-  document.getElementById('cursor').style.marginLeft = mouseX-15+"px";
+function initialise(){
+  setScore();
+  setLives();
+  setSpeed(target, 0.4);
+  detectTarget();
+  document.onmousemove = findMouse;
 }
 
-function findCollision(){
-  for(let i = 0; i < enemies.length; i++){
-    if( mouseX>=enemies[i].getBoundingClientRect().left &&
-      mouseX<=enemies[i].getBoundingClientRect().right &&
-      mouseY>=enemies[i].getBoundingClientRect().top &&
-      mouseY<=enemies[i].getBoundingClientRect().bottom &&
-      damaged === false)
-    {
-      clearInterval(collision);
-      damaged = true;
-      loseLife(enemies[i]);
-      setTimeout(function(){
-        damaged = false;
-        collision = setInterval(findCollision, 50);
-      }, 500);
+function detectTarget() {
+    setInterval(function () {
+        if (findCollision(target) === true) {
+            getPoint();
+        }
+    }, 50);
+}
+
+function findCollision(element) {
+    if (mouseX+10 >= element.getBoundingClientRect().left &&
+        mouseX+10 <= element.getBoundingClientRect().right &&
+        mouseY >= element.getBoundingClientRect().top &&
+        mouseY <= element.getBoundingClientRect().bottom) {
+        return true;
     }
+  if (mouseX-10 >= element.getBoundingClientRect().left &&
+      mouseX-10 <= element.getBoundingClientRect().right &&
+      mouseY >= element.getBoundingClientRect().top &&
+      mouseY <= element.getBoundingClientRect().bottom) {
+    return true;
   }
-  if( mouseX>=dead.getBoundingClientRect().left &&
-    mouseX<=dead.getBoundingClientRect().right &&
-    mouseY>=dead.getBoundingClientRect().top &&
-    mouseY<=dead.getBoundingClientRect().bottom)
-  {
-    loseGame();
+  if (mouseX >= element.getBoundingClientRect().left &&
+      mouseX <= element.getBoundingClientRect().right &&
+      mouseY+10 >= element.getBoundingClientRect().top &&
+      mouseY+10 <= element.getBoundingClientRect().bottom) {
+    return true;
   }
-  if( mouseX>=target.getBoundingClientRect().left &&
-      mouseX<=target.getBoundingClientRect().right &&
-      mouseY>=target.getBoundingClientRect().top &&
-      mouseY<=target.getBoundingClientRect().bottom)
-  {
-    clearInterval(collision);
-    damaged = true;
-    getPoint();
-    setTimeout(function(){
-      damaged = false;
-      collision = setInterval(findCollision, 50);
-    }, 100);
+  if (mouseX >= element.getBoundingClientRect().left &&
+      mouseX <= element.getBoundingClientRect().right &&
+      mouseY-10 >= element.getBoundingClientRect().top &&
+      mouseY-10 <= element.getBoundingClientRect().bottom) {
+    return true;
   }
-}
 
-for(let i = 0; i < enemies.length; i++){
-  enemies[i].style.transition = enemySpeed+"s";
-  setInterval(move, interval, enemies[i]);
-}
-
-checkScreen();
-
-function checkScreen(){
-  width = window.innerWidth;
-  height = window.innerHeight - barHeight;
-}
-
-function getWidth(element) {
-  return element.offsetWidth;
-}
-
-function getHeight(element) {
-  return element.offsetHeight;
-}
-
-function setLives(){
-  document.getElementById('lives').innerHTML = lives;
-}
-
-function setScore(){
-  document.getElementById('score').innerHTML = score;
-}
-
-function move(element) {
-  checkScreen();
-  let x = Math.floor(Math.random() * (width - (getWidth(element)/2)));
-  let y = Math.floor(Math.random() * (height - (getHeight(element)/2)));
-  element.style.marginLeft = x+"px";
-  element.style.marginTop = y+"px";
-}
-
-function spawn(element){
-  element.style.display = "block";
 }
 
 function getPoint() {
-  if(score > 5){
-    if(score < 175){
-      if(score % 10 === 0){
-        target.style.width = target.offsetWidth-10+"px";
-        target.style.height = target.offsetHeight-10+"px";
+    if(gotPoint === false){
+      score++;
+      setScore();
+      move(target);
+      gotPoint = true;
+      if (score % spawnEnemyInterval === 0) {
+        spawn("enemy", 2);
       }
+      if (score % spawnLifeInterval === 0) {
+        spawn("life", 1);
+      }
+      if (score === spawnBoss) {
+        spawn("boss", 3);
+      }
+      if(score % spawnSnitchInterval === 0){
+        spawn("snitch", 0.4);
+      }
+      if(score % spawnShieldInterval === 0){
+        spawn("shield", 5);
+      }
+      setTimeout(function () {
+        gotPoint = false;
+      }, 200);
     }
-  }
-  animate(target);
-  pointAnimate();
-  if(score < winScore){
-    score++;
-  }
+}
+
+function spawn(className, speed) {
+    let spawn = document.createElement('div');
+    spawn.classList.add(className);
+    document.getElementById('game').appendChild(spawn);
+    setSpeed(spawn, speed);
+    enemyMove(spawn);
+    setEffect(spawn);
+}
+
+function move(element) {
+    let x = Math.floor(Math.random() * (getScreenWidth() - getWidth(element)));
+    let y = Math.floor(Math.random() * (getScreenHeight() - getHeight(element)));
+    element.style.marginLeft = x + "px";
+    element.style.marginTop = y + "px";
+}
+
+function enemyMove(element) {
+    move(element);
+    setTimeout(function () {
+        if (lives > 0) {
+            enemyMove(element);
+        }
+      }, getSpeed(element) * 1000);
+}
+
+function setEffect(element) {
+    switch (element.className) {
+        case "enemy":
+            setInterval(function () {
+                if (findCollision(element) === true && immune === false) {
+                    loseLife();
+                    noCollision(500);
+                }
+            }, 50);
+            break;
+        case "boss":
+            setInterval(function () {
+                if (findCollision(element) === true && immune === false) {
+                    loseGame();
+                }
+            }, 50);
+            break;
+        case "life":
+            setInterval(function () {
+                if (findCollision(element) === true) {
+                    gainLife(element);
+                }
+            }, 50);
+            break;
+      case "snitch":
+        setInterval(function () {
+          if (findCollision(element) === true) {
+            catchSnitch(element);
+          }
+        }, 50);
+        break;
+      case "shield":
+        setInterval(function () {
+          if (findCollision(element) === true && immune === false) {
+            noCollision(shieldTime);
+            animateShield(shieldTime);
+            destroy(element);
+          }
+        }, 50);
+        break;
+    }
+}
+
+function noCollision(immuneTime) {
+    immune = true;
+    setTimeout(function () {
+        immune = false;
+    }, immuneTime)
+}
+
+function destroy(element) {
+    document.getElementById('game').removeChild(element);
+}
+
+function catchSnitch(element) {
+  score = score+50;
   setScore();
-  if(score < winScore){
-    move(target);
+  destroy(element);
+  animateSnitch();
+  let enemies = document.getElementsByClassName("enemy");
+  for(let i = 0;i<3; i++){
+    destroy(enemies[i]);
   }
-  if(score === winScore){
-    winGame();
-  }
-  if(score >= spawnDeadWhen){
-    spawn(dead);
-  }
-  if(score < (enemies.length*spawnInterval)-1){
-    if(score % spawnInterval === 0){
-      spawn(enemies[((score/spawnInterval)-1)]);
+}
+
+function gainLife(element) {
+    lives++;
+    setLives();
+    animateLife();
+    destroy(element);
+}
+
+function loseLife() {
+    lives--;
+    setLives();
+    animateDamage();
+    if (lives === 0) {
+        loseGame();
     }
-  }
 }
 
-function loseLife(element){
-  animate(element);
-  damageAnimate();
-  lives--;
-  setLives();
-  if(lives === 0){
-    loseGame();
-  }
+function loseGame() {
+  document.getElementById('highscore').innerHTML = "Your score was "+score+"!";
+  animateLoss();
 }
 
-function animate(element){
-  element.classList.add("animate");
-  setTimeout(function(){
-    element.classList.remove("animate");
+function animateLife() {
+  document.getElementById('body').style.background = "rgb(90,15,60)";
+  setTimeout(function () {
+    document.getElementById('body').style.background = "rgb(15,15,15)";
   }, 100);
 }
 
-function damageAnimate(){
-  document.getElementById("game").style.background = "red";
-  setTimeout(function(){
-    document.getElementById("game").style.background = "";
+function animateShield(shieldTime){
+  let animate = true;
+  let time = 5;
+  document.getElementById('counter').innerHTML = ""+time;
+  document.getElementById('cursor').classList.add('shielded');
+  countDown();
+  function countDown(){
+    setTimeout(function () {
+      time--;
+      document.getElementById('counter').innerHTML = ""+time;
+      if(animate === true){
+        countDown();
+      } else{
+        document.getElementById('counter').innerHTML = "";
+      }
+    }, 1000);
+  }
+  setTimeout(function () {
+    document.getElementById('cursor').classList.remove('shielded');
+    document.getElementById('counter').innerHTML = "";
+    animate = false;
+  }, shieldTime)
+}
+
+function animateSnitch() {
+  lives++;
+  setLives();
+  document.getElementById('body').style.background = "rgb(120,100,15)";
+  setTimeout(function () {
+    document.getElementById('body').style.background = "rgb(15,15,15)";
   }, 100);
 }
 
-function pointAnimate(){
-  document.getElementById("game").style.background = "green";
-  setTimeout(function(){
-    document.getElementById("game").style.background = "";
-  }, 70);
+function animateDamage() {
+  document.getElementById('body').style.background = "rgb(80,15,15)";
+  setTimeout(function () {
+    document.getElementById('body').style.background = "rgb(15,15,15)";
+  }, 100);
 }
 
-function loseGame(){
-  lives = 0;
-  setLives();
-  clearInterval(death);
-  clearInterval(collision);
-  dead.classList.add("yeet");
-  setTimeout(function(){
-    dead.classList.add("gameOver");
+function animateLoss(){
+  document.getElementById('overlayLoss').classList.remove('gone');
+  setTimeout(function () {
+    document.getElementById('overlayLoss').classList.add("overlay");
   }, 50);
-  setTimeout(function(){
-    document.getElementById("overlay").classList.remove('gone');
-    setTimeout(function(){
-      document.getElementById("overlay").classList.add('yeet');
-    }, 50);
-  }, 2000);
-  document.getElementById("overlaytext").innerHTML = "Game over, your score was "+score+"!";
+  document.getElementById('body').style.cursor = "default";
+  document.getElementById('cursor').style.display = "none";
 }
 
-function winGame(){
-  //dead.classList.add('gone');
-  for(let i = 0; i < enemies.length; i++){
-    enemies[i].classList.add('gone');
-  }
-  clearInterval(death);
-  clearInterval(collision);
-  target.classList.add("yeet");
-  document.getElementById('link').classList.add('winLink');
-  setTimeout(function(){
-    target.classList.add("gameWin");
-  }, 50);
-  setTimeout(function(){
-    document.getElementById("overlay").classList.remove('gone');
-    setTimeout(function(){
-      document.getElementById("overlay").classList.add('yeet');
-      document.getElementById("overlay").classList.add('overlayWin');
-    }, 50);
-  }, 2000);
-  document.getElementById("overlaytext").innerHTML = "You crazy son of a bitch, you did it! You won!";
+function findMouse(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    document.getElementById('cursor').style.marginLeft = mouseX-10+"px";
+    document.getElementById('cursor').style.marginTop = mouseY-10+"px";
+}
+
+function getScreenWidth() {
+    return window.innerWidth;
+}
+
+function getScreenHeight() {
+    return window.innerHeight;
+}
+
+function getWidth(element) {
+    return element.offsetWidth;
+}
+
+function getHeight(element) {
+    return element.offsetHeight;
+}
+
+function getSpeed(element) {
+    let speed = element.style.transition.replace('all ', '');
+    return speed.replace('s ease 0s', '');
+}
+
+function setScore() {
+    document.getElementById('score').innerHTML = "" + score;
+}
+
+function setLives() {
+    document.getElementById('lives').innerHTML = "" + lives;
+}
+
+function setSpeed(element, speed) {
+    element.style.transition = speed + "s";
 }
